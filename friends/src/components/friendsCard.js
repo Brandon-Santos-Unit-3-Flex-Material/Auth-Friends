9cf0fd18b460389bf9d11c0f8340fe5950c4310f
brandon santos <brandonx1123@gmail.com>
2021-02-24 01:30:25 -0500

    mvp done for flex

---
+++ friends/src/components/friendsCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function FriendsCard({ friend }) {
+  return (
+    <div key={friend.id}>
+      <h3>Friend:{friend.name}</h3>
+      <p>Age: {friend.age}</p>
+      <p>Email: {friend.email} </p>
+    </div>
+  );
+}
+
+export default FriendsCard;
